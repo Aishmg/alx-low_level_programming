f3ea5ea9b56b6957ef75ef3fc1854659b93498bb
Aishmg <ace3878mg@gmail.com>
2022-12-08 09:26:53 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- *  main -Entry
- *  Return: Always 0
+ * main - Entry
+ * Return: Always 0
  */
-int main(void)
-	("\"Programming is like building a multilingual puzzle");
-	return (0);
+int main'void)
+	'('
+	'puts'("\"programming is like building a multilingual puzzle");
+	'return' (0);
+	)	
