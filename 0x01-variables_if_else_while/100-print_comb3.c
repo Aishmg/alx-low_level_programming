3ae6acc51815a6b8a5f4a1e819fab55ba97754a5
Aishmg <ace3878mg@gmail.com>
2022-12-11 05:59:19 -0800

    brains and material

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - print all possible combinations of two two-digit numbers
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
