a768b22c356493f66d000dfabd16c10056a75141
Aishmg <ace3878mg@gmail.com>
2022-12-19 11:25:31 -0800

    task four print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ * @s: string to be printed in reverse
+ *
+ * Return: nothing on success
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+
+	while (count)
+	{
+		s--;
+		_putchar(*s);
+		count++
+	}
+
+	_putchar('\n');
+}
