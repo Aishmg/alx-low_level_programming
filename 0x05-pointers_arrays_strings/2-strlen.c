788bbb2ce9cc1ce412f2cd81966a9ea619c2d21a
Aishmg <ace3878mg@gmail.com>
2022-12-19 11:08:33 -0800

    task two str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string whose lenght is to be found
+ *
+ * Returns: the lenght
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
