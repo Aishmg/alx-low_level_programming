d2fa0bb4367b18de391bd2364ac90ecbb24b8358
Aishmg <ace3878mg@gmail.com>
2022-12-21 03:48:27 -0800

    one swap task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int - swapping the value of two integers
- * @a: points to one value
- * @b: points to the other value
- * Return: void
+ * swap_int - swaps the value of two integers
+ * @a: the first integer to be swapped
+ * @b: the second integer to be swapped
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
+/* the function that swaps the values of two integers. */
 {
 	int temp = *a;
 	*a = *b;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoid(char *str);
 
-#enfif /*MAIN_H*/
+#endif /*MAIN_H*/
